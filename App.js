038efe763486b7a047b9a25356e7630ce122e746
Zain Ul Abideen <zain@supertal.io>
2023-02-25 11:12:55 +0500

    push notification

--- App.js
+++ App.js
@@ -1,16 +1,57 @@
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import React from 'react';
+import React, {useEffect} from 'react';
 import SplashScreen from './src/screens/splash';
 import SiginInScreen from './src/screens/signin';
 import SignUpScreen from './src/screens/SignUp';
 import SideNavigation from './src/screens/SideNavigation';
 import {Toast} from 'react-native-toast-message/lib/src/Toast';
 import OnBoardingScreen from './src/screens/Onboarding';
+import messaging from '@react-native-firebase/messaging';
+import {PermissionsAndroid, Alert} from 'react-native';
 
 const stack = createNativeStackNavigator();
 
 const App = () => {
+  useEffect(() => {
+    pushNotification();
+
+    const unsubscribe = messaging().onMessage(async remoteMessage => {
+      console.log('FCM message arrived 101!', JSON.stringify(remoteMessage));
+    });
+
+    messaging().setBackgroundMessageHandler(async remoteMessage => {
+      console.log('Message handled in the background!', remoteMessage);
+    });
+
+    messaging().onNotificationOpenedApp(remoteMessage => {
+      console.log(
+        'App Open By Clicking Notification',
+        remoteMessage.notification,
+      );
+    });
+
+    messaging()
+      .getInitialNotification()
+      .then(remoteMessage => {
+        if (remoteMessage) {
+          console.log(
+            'Notification caused app to open from quit state:',
+            remoteMessage,
+          );
+        }
+      });
+
+    return unsubscribe;
+  }, []);
+
+  async function pushNotification() {
+    let fcmToken = await messaging().getToken();
+    if (fcmToken) {
+      console.log('token', fcmToken);
+    }
+  }
+
   return (
     <>
       <NavigationContainer>
